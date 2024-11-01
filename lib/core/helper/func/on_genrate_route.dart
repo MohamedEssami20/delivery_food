e68d6a966fed363fa92c023a6f4b3d53b1b68476
mohamed essam <mohamedessam2003147@gmail.com>
2024-11-01 19:34:06 +0200

    create splash view  and on generate root method

---
+++ lib/core/helper/func/on_genrate_route.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:delivery_food/features/splash/presentation/views/splash_view.dart';
+
+Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    // Add more routes here as needed
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
