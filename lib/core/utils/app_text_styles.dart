890eaa3e0c13483c13093447ea9225bcf996477b
mohamed essam <mohamedessam2003147@gmail.com>
2024-11-01 20:01:14 +0200

    add font family and app text style

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle medium14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 20 / 14,
+  );
+
+  static const TextStyle semiBold14 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 20 / 14,
+  );
+
+  static const TextStyle medium12 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 16 / 12,
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle bold16 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle semiBold16 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 24 / 16,
+  );
+
+  static const TextStyle medium10 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    fontSize: 10,
+    height: 16 / 10,
+  );
+
+  static const TextStyle semiBold10 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 10,
+    height: 16 / 10,
+  );
+
+  static const TextStyle bold2 = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 2,
+    height: 3.4,
+  );
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/app_text_styles.dart';
+
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text(
+        "mohameeddddddd",
+        style: AppTextStyles.medium14.copyWith(fontSize: 25),
+      ),
+    );
   }
 }
