2e95bf63a9469013b8990b286b1395bc5b5e2c3e
mohamed essam <mohamedessam2003147@gmail.com>
2024-11-04 21:27:23 +0200

    add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:delivery_food/core/service/register_shared_pref.dart';
 import 'package:delivery_food/features/splash/presentation/views/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/helper/func/on_genrate_route.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   // registerSharedPref(); 
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
  await SharedPrefService().registerSharedPref();
   runApp(const DeliveryFood());
 }
