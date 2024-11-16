27badfa1f9b4d6ad63115c17400d4e162124be6e
mohamed essam <mohamedessam2003147@gmail.com>
2024-11-16 10:51:50 +0200

    create bottom navigation bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,8 @@
+
 import 'package:delivery_food/features/home/presentation/manager/category_cubit/category_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../core/utils/app_color.dart';
 import 'widgets/home_view_body.dart';
@@ -32,11 +34,15 @@ class BottomNavigationBarWidget extends StatelessWidget {
       backgroundColor: Colors.white,
       selectedItemColor: AppColor.primaryColor,
       unselectedItemColor: Colors.black54,
-      items: [
+      items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.list), label: "My Orders"),
+        BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.bagShopping), label: "Shopping"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble), 
+            label: "Chat"),
         BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
       ],
     );
-  }
+  } 
 }
