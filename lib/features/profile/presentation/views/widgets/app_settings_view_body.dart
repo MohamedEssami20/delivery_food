58976c94eee3e4e84ed83d96599de0239b0a54a5
mohamed essam <mohamedessam2003147@gmail.com>
2024-11-29 21:00:58 +0200

    create setting switch widget

--- lib/features/profile/presentation/views/widgets/app_settings_view_body.dart
+++ lib/features/profile/presentation/views/widgets/app_settings_view_body.dart
@@ -1,10 +1,44 @@
+import 'package:delivery_food/core/utils/app_text_styles.dart';
+import 'package:delivery_food/core/utils/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'settings_switch_item.dart';
 
 class AppSettingsViewBody extends StatelessWidget {
   const AppSettingsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      top: true,
+      minimum: const EdgeInsets.only(top: 30),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomAppBar(actions: SizedBox(), title: "Settings"),
+            const SizedBox(height: 50),
+            Text(
+              'PROFILE',
+              style: AppTextStyles.medium14.copyWith(
+                color: const Color(0xFF878787),
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const SettingsSwitchItem(
+              value: true,
+              title: 'Push Notification',
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const SettingsSwitchItem(
+              value: false,
+              title: "Location",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/profile/presentation/views/widgets/settings_switch_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_color.dart';
+import '../../../../../core/utils/app_text_styles.dart';
+
+class SettingsSwitchItem extends StatelessWidget {
+  const SettingsSwitchItem({super.key, required this.value, required this.title});
+  final bool value;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: SwitchListTile(
+            title: Text(
+              title,
+              style: AppTextStyles.medium14.copyWith(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+            ),
+            value: value,
+            onChanged: (val) {},
+            activeColor: Colors.white,
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: const Color(0xffDCDEDE),
+            activeTrackColor: AppColor.primaryColor,
+            trackOutlineColor: const WidgetStatePropertyAll(Colors.transparent),
+            thumbIcon: const WidgetStatePropertyAll(
+              Icon(
+                Icons.circle,
+                color: Colors.white,
+              ),
+            ),
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+        ),
+      ],
+    );
+  }
+}
